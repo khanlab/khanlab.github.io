3bb548a99b66700541709b06c27d0ddf651aa551
Jason Kai <tkai@uwo.ca>
2021-05-20 19:14:01 -0400

    update partners logos

--- js/custom.js
+++
@@ -1,7 +0,0 @@
-// Navbar
-const navbar = document.querySelector("#navbar");
-const menuToggle = document.querySelector("#nav-toggle");
-
-menuToggle.addEventListener("click", () => {
-  navbar.classList.toggle("open");
-});
