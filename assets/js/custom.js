2652e5479a224c197899af323b11c1c3a939bdde
Jason Kai <tkai@uwo.ca>
2021-05-26 11:57:04 -0400

    add page transition

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,3 +4,16 @@ $(document).ready(function () {
     $(".animated-icon").toggleClass("open");
   });
 });
+
+// Page transition
+$(document).ready(function() {
+  $("body").show(500);
+  $("a").click(function() {
+      $link = $(this).attr("href");
+      setTimeout(function(){
+          window.location.replace($link);
+      },900);
+      $("body").hide(1000);
+      return false;
+  });
+});
