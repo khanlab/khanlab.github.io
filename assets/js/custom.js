559f506b8d30f3edff90f32d3e20fe70d4ab5b64
Jason Kai <tkai@uwo.ca>
2021-08-17 18:03:00 -0400

    remove page transition to fix popup bug

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
   });
 });
 
-// Page transition
-$(document).ready(function() {
-  $("body").show(500);
-  $("a").click(function() {
-      $link = $(this).attr("href");
-      setTimeout(function(){
-          window.location.replace($link);
-      },900);
-      $("body").hide(1000);
-      return false;
-  });
-});
+// Page transition - currently breaks open new target window 
+// $(document).ready(function() {
+//   $("body").show(500);
+//   $("a").click(function() {
+//       $link = $(this).attr("href");
+//       setTimeout(function(){
+//           window.location.replace($link);
+//       },900);
+//       $("body").hide(1000);
+//       return false;
+//   });
+// });
